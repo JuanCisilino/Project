1ca41f5ae41b53450cea10ec17ba18532dff13cd
Juan Cisilino <54241344+JuanCisilino@users.noreply.github.com>
2022-07-19 09:49:06 -0300

    Cross Origin correction

--- project_wm/app/src/main/java/com/frost/project_wm/model/UserUpdateRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.frost.project_wm.model
-
-data class UserUpdateRequest(
-    var email: String,
-    var rol: String
-)
--- src/main/kotlin/com/projectwm/api/controller/Controller.kt
+++ src/main/kotlin/com/projectwm/api/controller/Controller.kt
@@ -2,7 +2,6 @@ package com.projectwm.api.controller
 
 import com.projectwm.api.models.ProductoLocal
 import com.projectwm.api.models.UsuarioLocal
-import com.projectwm.api.requests.ProductRequest
 import com.projectwm.api.requests.UserRequest
 import com.projectwm.api.services.ProductServiceImpl
 import com.projectwm.api.services.UserServiceImpl
@@ -19,58 +18,71 @@ class Controller {
     private val userService = UserServiceImpl()
     private val productService = ProductServiceImpl()
 
-    @CrossOrigin("https://project-wm.herokuapp.com")
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/users/{email}")
     @ResponseBody
     suspend fun getByEmail(@PathVariable email: String) =
         ResponseEntity(userService.getByEmail(email), HttpStatus.OK)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/users")
     suspend fun paginatedUserList(@RequestParam pageNo: Int, @RequestParam pageSize: Int,@RequestParam by: String) =
         userService.getPageBy(pageNo, pageSize, by)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/initUsers")
     suspend fun getAllUsers() = userService.getAll()
 
+    @CrossOrigin(origins = ["*"])
     @PatchMapping("/users")
     suspend fun updateUser(@RequestBody @Valid user: UserRequest) =
         userService.updateUser(user)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/users")
     suspend fun createUser(@RequestBody @Valid user: UsuarioLocal) =
         userService.createUser(user)
 
+    @CrossOrigin(origins = ["*"])
     @DeleteMapping("/users/{email}")
     suspend fun deleteUser(@PathVariable email: String) =
         userService.deleteUser(email)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/prodByTitle/{title}")
     suspend fun getByTitle(@PathVariable title: String) =
         ResponseEntity(productService.getByTitle(title), HttpStatus.OK)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/products")
     suspend fun paginatedProductList(@RequestParam pageNo: Int, @RequestParam pageSize: Int) =
         productService.getPageBy(pageNo, pageSize)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/prodByCompany/{company}")
     suspend fun getByCompany(@PathVariable company: String) = productService.getByCompany(company)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/prodById/{id}")
     @ResponseBody
     suspend fun getById(@PathVariable id: Int) =
         ResponseEntity(productService.getById(id), HttpStatus.OK)
 
+    @CrossOrigin(origins = ["*"])
     @GetMapping("/initProducts")
     suspend fun getAllProducts() = productService.getAllProducts()
 
+    @CrossOrigin(origins = ["*"])
     @PatchMapping("/products")
     suspend fun updateProd(@RequestBody @Valid product: ProductoLocal) =
         productService.updateProduct(product)
 
+    @CrossOrigin(origins = ["*"])
     @PostMapping("/products")
     suspend fun createProduct(@RequestBody @Valid product: ProductoLocal) =
         productService.createProduct(product)
 
+    @CrossOrigin(origins = ["*"])
     @DeleteMapping("/products/{id}")
     suspend fun deleteProduct(@PathVariable id: Int) =
         productService.deleteProduct(id)
